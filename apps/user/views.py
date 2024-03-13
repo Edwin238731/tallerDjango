90a4cde68b2201edb3df705100fd3977370d45c2
Edwin polanco <edwindejesuspolancocob@gmail.com>
2024-03-13 17:47:56 -0600

    correccion de errores

--- apps/user/views.py
+++ apps/user/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class LoginView(TemplateView):
-  template_name: 'user/index.html'
+  template_name = 'user/index.html'
