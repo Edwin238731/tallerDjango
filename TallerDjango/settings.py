9a3e3dba8e2ea5344c1cab1b675066c24b970809
Edwin polanco <edwindejesuspolancocob@gmail.com>
2024-03-14 17:57:48 -0600

    registro y envio de correo

--- TallerDjango/settings.py
+++ TallerDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =( 
+STATICFILES_DIRS =(
   os.path.join(BASE_DIR, 'static'),
   )
 
@@ -130,3 +133,23 @@ STATICFILES_DIRS =(
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+######### Copiar al final ###############
+
+AUTH_USER_MODEL = 'user.User'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.gocontrol.mx'
+EMAIL_PORT = '465'
+EMAIL_HOST_USER = 'tallerdjango@gocontrol.mx'
+EMAIL_HOST_PASSWORD = 'L9Y8HPSapinK'
+EMAIL_USE_SSL = True
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
+
+TEXTBOXMAXLENGTH = 50
